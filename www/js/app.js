933d857da62013c4132eff9a7a6e2d98d26d0000
ygalavay <ygalavay@gmail.com>
2016-02-15 17:53:00 +0100

    Added displaying list of prays

--- www/js/app.js
+++ www/js/app.js
@@ -10,8 +10,11 @@
         };
     });
 
-    module.controller('DetailController', function ($scope) {
+    module.controller('DetailController', function ($scope, Services) {
         $scope.category = $scope.navi.getCurrentPage().options.category;
+        Services.getPraysForCategory($scope.category.id, function(data) {
+            $scope.prays = data;
+        });
     });
 
     module.controller('MasterController', ['$scope', '$http', 'Services', function ($scope, $http, Services) {
@@ -26,16 +29,26 @@
     }]);
 
     module.factory('Services', function ($http) {
+        var siteUrl = 'http://rest.prayer.com.ua/rest';
+        var doGET = function (url, onSuccess, onError) {
+            var responsePromise = $http.get(url);
+            onError = onError || function () {
+                alert("Problem loading data");
+            };
+            onSuccess = onSuccess || function(){};
+            responsePromise.success(onSuccess);
+            responsePromise.error(onError);
+        };
         return {
             getAllCategories: function (onSuccess, onError) {
-                var responsePromise = $http.get("http://rest.prayer.com.ua/rest/category");
-                onError = onError || function () {
-                    alert("Problem loading data");
-                 };
-                responsePromise.success(onSuccess);
-                responsePromise.error(onError);
+                doGET(siteUrl + '/category', onSuccess, onError);
+            },
+
+            getPraysForCategory : function(categoryId, onSuccess, onError) {
+                var url = siteUrl + '/pray/list?category=' + categoryId;
+                doGET(url, onSuccess, onError);
             }
-        }
+        };
     });
 
 })();
