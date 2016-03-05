6bb3c6b3e92c114fa2a87f294015957aabbdd092
ygalavay <ygalavay@gmail.com>
2016-03-05 12:47:27 +0100

    Extracted code between deifferent modules

--- www/js/app.js
+++ www/js/app.js
@@ -1,55 +1,7 @@
-var context = {
-	systemproperties: {
-		keys: {
-			firstLaunch: 'firstLaunch',
-			religion: 'religion',
-			language: 'language',
-			scaling: 'scaling'
-		},
-
-		setValue: function (key, value) {
-			localStorage.setItem(key, value);
-		},
-
-		getValue: function (key, defaultValue) {
-			return localStorage.getItem(key) || defaultValue;
-		}
-	},
-
-	storage_name : 'prays-storage'
-};
-
 (function () {
     'use strict';
 
-	var calculateTextSizes = function() {
-		var scaling_middle = 50;
-		var scaling = context.systemproperties.getValue(context.systemproperties.keys.scaling, 50);
-		var delta = (scaling - scaling_middle) / 50; /*delta value*/
-
-		var getValueMinusDeltaPercentage = function(value, delta, scalingRate) {
-			//scalingRate = scalingRate | 0.2; //TODO: Default value
-			var scalingMaximumValue = value * scalingRate;
-
-			if (delta < 0) {
-				return Math.round(value - (scalingMaximumValue * delta * -1));
-			}
-			else if (delta == 0) {
-				return value;
-			} else {
-				return Math.round(value + (scalingMaximumValue * delta));
-			}
-		};
-
-		return {
-			'pray_title_font_size' : getValueMinusDeltaPercentage(18, delta, 0.3) + 'px',
-			'pray_content_font_size' : getValueMinusDeltaPercentage(16, delta, 0.4) + 'px',
-			'menu_item_font_size' : getValueMinusDeltaPercentage(18, delta, 0.3),
-			'navigation_bar_font_size' : getValueMinusDeltaPercentage(18, delta, 0.3)
-		}
-	};
-
-    var module = angular.module('prayer', ['onsen', 'ngSanitize']);
+    var module = angular.module('prayer');
 
     module.controller('AppController', function ($scope) {
 			if (!context.systemproperties.getValue(context.systemproperties.keys.language)) {
@@ -85,6 +37,10 @@ var context = {
         $scope.showPrayItem = function (pray) {
             navi.pushPage('pray-item-view.html', {prayItemId: pray.id});
         };
+
+			$scope.addItemToFavorites = function(pray) {
+				console.log(pray.id + " is added to favorites");
+			}
     });
 
     module.controller('PraysItemViewController', function ($scope, Services) {
@@ -133,37 +89,5 @@ var context = {
 				navi.popPage();
 			}
 		});
-
-    module.factory('Services', function ($http, $timeout) {
-        var siteUrl = 'http://rest.prayer.com.ua/rest';
-        var doGET = function (url, onSuccess, onError) {
-            var responsePromise = $http.get(url);
-            onError = onError || function () {
-                alert("Problem loading data");
-            };
-            onSuccess = onSuccess || function(){};
-            $timeout(function(){
-                responsePromise.success(onSuccess);
-            },'700');
-            responsePromise.error(onError);
-        };
-        return {
-            getAllCategories: function (onSuccess, onError) {
-							var language = context.systemproperties.getValue(context.systemproperties.keys.language, 'UA');
-                doGET(siteUrl + '/category?language=' + language, onSuccess, onError);
-            },
-
-            getPraysForCategory : function(categoryId, onSuccess, onError) {
-                var url = siteUrl + '/pray/list?category=' + categoryId;
-                doGET(url, onSuccess, onError);
-            },
-
-            getPrayItemById : function(prayItemId, onSuccess, onError) {
-                var url = siteUrl + '/pray/' + prayItemId;
-                doGET(url, onSuccess, onError);
-            }
-        };
-    });
-
 })();
 
---
+++ www/js/services.js
@@ -0,0 +1,34 @@
+(function () {
+	'use strict';
+	var module = angular.module('prayer');
+	module.factory('Services', function ($http, $timeout) {
+		var siteUrl = 'http://rest.prayer.com.ua/rest';
+		var doGET = function (url, onSuccess, onError) {
+			var responsePromise = $http.get(url);
+			onError = onError || function () {
+					alert("Problem loading data");
+				};
+			onSuccess = onSuccess || function(){};
+			$timeout(function(){
+				responsePromise.success(onSuccess);
+			},'700');
+			responsePromise.error(onError);
+		};
+		return {
+			getAllCategories: function (onSuccess, onError) {
+				var language = context.systemproperties.getValue(context.systemproperties.keys.language, 'UA');
+				doGET(siteUrl + '/category?language=' + language, onSuccess, onError);
+			},
+
+			getPraysForCategory : function(categoryId, onSuccess, onError) {
+				var url = siteUrl + '/pray/list?category=' + categoryId;
+				doGET(url, onSuccess, onError);
+			},
+
+			getPrayItemById : function(prayItemId, onSuccess, onError) {
+				var url = siteUrl + '/pray/' + prayItemId;
+				doGET(url, onSuccess, onError);
+			}
+		};
+	});
+})();
