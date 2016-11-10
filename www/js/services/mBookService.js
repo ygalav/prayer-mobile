88d72e6f331160c8e4cafb023138c7d19ca8865e
ygalavay <ygalavay@gmail.com>
2016-11-10 17:07:53 +0100

    Fixed hardcoded default language id for Russian language

--- www/js/services/mBookService.js
+++ www/js/services/mBookService.js
@@ -6,8 +6,10 @@
 		return {
 			getDefaultBookIDForCurrentLanguage: function () {
 				switch (prLanguageService.getCurrentLanguage()) {
+					//TODO: It would be great to have all this functionality on remote
 					case 'UA' : return 1;
 					case 'PL' : return 3;
+					case 'RU' : return 4;
 					default : return 2;
 				}
 			}
